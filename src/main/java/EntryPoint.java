5d16c79edca30d990abd159a523adff4f9cb2144
Anastasiia Kabeshova <kabeshovaas@gmail.com>
2014-08-21 07:51:29 +0200

    ignore xls files + change input file

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,5 +1,4 @@
 
-import java.awt.Color;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
@@ -9,13 +8,10 @@ import java.util.logging.Logger;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.Marker;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.ValueMarker;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
-import org.jfree.ui.TextAnchor;
 
 /**
  *
